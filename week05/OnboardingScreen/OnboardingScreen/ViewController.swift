5bc61cbabf6878382e0d96488a1d3a3ab79b1c51
Humeyra Sahin <58955580+humeyrasahin7@users.noreply.github.com>
2022-02-06 22:31:32 +0300

    onboarding screen done

---
+++ week05/OnboardingScreen/OnboardingScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingScreen
+//
+//  Created by Hümeyra Şahin on 6.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

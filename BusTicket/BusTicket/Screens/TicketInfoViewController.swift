5c9b2a180a9be3d22f9ebf306c9839698addb4f6
Humeyra Sahin <bme.humeyrasahin@outlook.com>
2022-02-15 20:22:20 +0300

    alert controller bg set, price tag added

--- BusTicket/BusTicket/Screens/TicketInfoViewController.swift
+++ BusTicket/BusTicket/Screens/TicketInfoViewController.swift
@@ -23,7 +23,12 @@ class TicketInfoViewController: UIViewController {
     @IBOutlet weak var passengerInfoLabel: UILabel!
     @IBOutlet weak var reservedSeatsLabel: UILabel!
     
+    @IBOutlet weak var priceView: UIView!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     var ticket: Ticket!
+    var hour: String!
+    var minute: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,20 +36,45 @@ class TicketInfoViewController: UIViewController {
         infoMainView.layer.cornerRadius = 30
         destinationView.layer.cornerRadius = 30
         passengerInfoView.layer.cornerRadius = 30
+        priceView.layer.cornerRadius = 30
+        
+        fromLabel.adjustsFontSizeToFitWidth = true
+        fromLabel.minimumScaleFactor = 0.7
+        toLabel.adjustsFontSizeToFitWidth = true
+        toLabel.minimumScaleFactor = 0.7
+        
+        reservedSeatsLabel.adjustsFontSizeToFitWidth = true
+        reservedSeatsLabel.minimumScaleFactor = 0.7
+        
+        priceLabel.adjustsFontSizeToFitWidth = true
+        priceLabel.minimumScaleFactor = 0.7
         
         fromLabel.text = ticket.from?.uppercased()
         toLabel.text = ticket.to?.uppercased()
+        
+        if ticket.clock.hour == 0{
+            hour = "00"
+        } else{
+            hour = "\(ticket.clock.hour)"
+        }
+        
+        if ticket.clock.minute == 0{
+            minute = "00"
+        } else {
+            minute = "\(ticket.clock.minute)"
+        }
         
         var reservedChairs = "\(ticket.selectedChairs!.sorted())"
         reservedChairs.remove(at: reservedChairs.startIndex)
         reservedChairs.remove(at: (reservedChairs.index(before: reservedChairs.endIndex)))
         
-        dateInfoLabel.text = "\(ticket.date.day)/\(ticket.date.month)/\(ticket.date.year) \(ticket.clock.hour):\(ticket.clock.minute)"
+        dateInfoLabel.text = "\(ticket.date.day)/\(ticket.date.month)/\(ticket.date.year) - \(hour!) : \(minute!)"
         
-        passengerInfoLabel.text = "\(ticket.passenger.name.uppercased()) \(ticket.passenger.surname.uppercased())"
+        passengerInfoLabel.text = "\(ticket.passenger.name.capitalized) \(ticket.passenger.surname.capitalized)"
         
         reservedSeatsLabel.text = reservedChairs
-                
+    
+        priceLabel.text = "\(ticket.selectedChairCount! * 80)₺"
     }
    
 

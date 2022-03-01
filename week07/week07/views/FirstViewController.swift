f828cff52e92421afde6ce139f63ff212f0a7ea1
Humeyra Sahin <bme.humeyrasahin@outlook.com>
2022-03-01 21:23:14 +0300

    custom tabbar done

---
+++ week07/week07/views/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  week07
+//
+//  Created by Hümeyra Şahin on 1.03.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    func configTabbar(){
+        
+    }
+
+
+}
---
+++ week07/week07/views/ThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdViewController.swift
+//  week07
+//
+//  Created by Hümeyra Şahin on 1.03.2022.
+//
+
+import UIKit
+import MapKit
+class ThirdViewController: UIViewController {
+
+    @IBOutlet weak var adressLabel: UILabel!
+    @IBOutlet weak var mapKit: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}

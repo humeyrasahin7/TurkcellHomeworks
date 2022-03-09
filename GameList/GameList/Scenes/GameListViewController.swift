fad4670fb8ff26ac5632a4e19de7e8b7b0805b36
Humeyra Sahin <bme.humeyrasahin@outlook.com>
2022-03-10 00:19:48 +0300

    Auto slide added

--- GameList/GameList/Scenes/GameListViewController.swift
+++ GameList/GameList/Scenes/GameListViewController.swift
@@ -15,7 +15,7 @@ class GameListViewController: UIViewController {
     
     private var downLoadedImageList = [Int:UIImage]()
     
-    var currentPageIndext = 0
+    var currentPageIndex = 0
     var image = UIImage()
     var games = Games(){
         didSet{
@@ -29,6 +29,7 @@ class GameListViewController: UIViewController {
                         self.downLoadedImageList[(result?.id)!] = image
                         self.collectionView.reloadData()
                         self.bannerCollectionView.reloadData()
+                        
                     })
                 }
             }
@@ -47,7 +48,9 @@ class GameListViewController: UIViewController {
         initCollectionView(collectionView: collectionView)
         initCollectionView(collectionView: bannerCollectionView)
        
+        Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(self.imageChange), userInfo: nil, repeats: true)
         pageController.addTarget(self, action: #selector(pageControlValueChanged(_:)), for: .valueChanged)
+        
         gameRequest.getGames { result in
             
             switch result{
@@ -55,21 +58,30 @@ class GameListViewController: UIViewController {
                 self.games = games
             case .failure(let error):
                 print(error.localizedDescription)
-               
+
     
             }
         }
+    }
+    
+    @objc func imageChange(_ sender: UIPageControl){
         
-       
+        if currentPageIndex < 2{
+            currentPageIndex += 1
+        } else {
+            currentPageIndex = 0
+        }
+        let indexPath = IndexPath.init(row: currentPageIndex, section: 0)
+        self.bannerCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        pageController.currentPage = currentPageIndex
     }
 
     @objc func pageControlValueChanged(_ sender: UIPageControl) {
         let index = sender.currentPage
         let indexPath = IndexPath.init(row: index, section: 0)
-        //let direction: UIPageViewController.NavigationDirection = currentPageIndext > index ? .reverse : .forward
-        currentPageIndext = index
+        currentPageIndex = index
         self.bannerCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-        pageController.currentPage = currentPageIndext
+        pageController.currentPage = currentPageIndex
     }
     
     func initCollectionView(collectionView: UICollectionView){
@@ -124,11 +136,12 @@ extension GameListViewController: UICollectionViewDataSource, UICollectionViewDe
         }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard let cell = bannerCollectionView.visibleCells.first else {return}
-        let indexPath = bannerCollectionView.indexPath(for: cell)
-        currentPageIndext = indexPath!.row
-            print(currentPageIndext)
-        
+        if scrollView == bannerCollectionView{
+        let offSet = scrollView.contentOffset.x
+        let width = scrollView.frame.width
+        let horizontalCenter = width / 2
+        pageController.currentPage = Int(offSet + horizontalCenter) / Int(width)
+        }
     }
 }
 
